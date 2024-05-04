111b4b31d41c78d31cefbe004644d35abfa271d0
HarshDhaduk2001 <107307409+HarshDhaduk2001@users.noreply.github.com>
2024-05-04 09:49:58 +0530

    udamy course investment calculator

---
+++ investment_calculator/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Invested Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Printf("Future Value: %v\nFuture Value (adjusted for Inflation): %v\n", futureValue, futureRealValue)
+	fmt.Printf("Future Value: %.0f\nFuture Value (adjusted for Inflation): %.1f\n", futureValue, futureRealValue)
+}
