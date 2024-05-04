6e222100ba6ce601f39ed740898c96d6ae2b7381
HarshDhaduk2001 <107307409+HarshDhaduk2001@users.noreply.github.com>
2024-05-04 09:44:13 +0530

    udamy course profit calculator

---
+++ pointers/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32
+	var agePointer *int = &age
+	fmt.Println("Age:", *agePointer)
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age)
+}
+
+func editAgeToAdultYears(age *int) {
+	*age = *age - 18
+}
