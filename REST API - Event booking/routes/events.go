428353e867dff3282f0d3a5e6e04ba5d9fb6d0e3
HarshDhaduk2001 <107307409+HarshDhaduk2001@users.noreply.github.com>
2024-05-09 21:52:36 +0530

    event booking getAllEvent api created

--- REST API - Event booking/routes/events.go
+++ REST API - Event booking/routes/events.go
@@ -7,6 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events."})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
 func createEvent(context *gin.Context) {
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
