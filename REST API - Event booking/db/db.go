5cdef26f68301f1a2a3fda868e5c44cbb6d5ce58
HarshDhaduk2001 <107307409+HarshDhaduk2001@users.noreply.github.com>
2024-05-09 21:31:34 +0530

    event booking event model and event create api done

--- REST API - Event booking/db/db.go
+++ REST API - Event booking/db/db.go
@@ -57,4 +57,22 @@ func createTables() {
 	if err != nil {
 		panic("Could not create events table: " + err.Error())
 	}
+
+	createEventsTable := `
+    CREATE TABLE IF NOT EXISTS events (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        name TEXT NOT NULL,
+        description TEXT NOT NULL,
+        location TEXT NOT NULL,
+        dateTime DATETIME NOT NULL,
+        user_id INTEGER,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+    `
+
+	_, err = DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic("Could not create events table: " + err.Error())
+	}
 }
