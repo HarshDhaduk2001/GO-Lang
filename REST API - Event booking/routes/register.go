efb8efb5b3f205d7f4b8d2df0cca08c948dae575
HarshDhaduk2001 <107307409+HarshDhaduk2001@users.noreply.github.com>
2024-05-11 21:17:46 +0530

    event booking cancel event api created

--- REST API - Event booking/routes/register.go
+++ REST API - Event booking/routes/register.go
@@ -33,3 +33,24 @@ func registerForEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Registered!"})
 }
+
+func cancelRegistration(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id." + err.Error()})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration." + err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Cancelled!"})
+}
